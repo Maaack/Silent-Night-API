18739d9bf2b7e71f27a3a1f5d3f962bd55d19009
Marek Belski <marek@webounty.com>
2016-02-07 16:22:34 -0800

    Game API - Settings, Space, and Snapshots started.

--- game_api/models/models.py
+++ game_api/models/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils.translation import ugettext, ugettext_lazy as _
 
-from silent_night.mixins.models import TimeStamped, Owned
+from silent_night.mixins.models import TimeStamped, Owned, Ownable
 from django.contrib.postgres.fields import jsonb
 
 
@@ -26,10 +26,39 @@ class Game(TimeStamped):
         return self.name + " | " + self.code
 
 
-class GameSettings(TimeStamped):
+class Settings(TimeStamped, Ownable):
+    class Meta:
+        abstract = True
+        ordering = ["-created"]
+
+    name = models.CharField(_("Name"), max_length=50, blank=True, null=True)
+    data = jsonb.JSONField(_("Data"), default={})
+
+
+class GameSettings(Settings):
     class Meta:
         verbose_name = _("Game Settings")
         verbose_name_plural = _("Game Settings")
+
+
+class Snapshot(TimeStamped):
+    state = jsonb.JSONField(_("Game State"), default={})
+    game_time = models.FloatField(_("Game Time in Seconds"), default=0.0)
+
+
+class Space(TimeStamped):
+    class Meta:
+        verbose_name = _("Space")
+        verbose_name_plural = _("Spaces")
         ordering = ["-created"]
 
-    data = jsonb.JSONField(default={})
+    game = models.ForeignKey("Game")
+    initial_snapshot = models.ForeignKey("Snapshot")
+    settings = models.ForeignKey("SpaceSettings")
+    seed = models.CharField(_("Random Seed"), max_length=50)
+
+
+class SpaceSettings(Settings):
+    class Meta:
+        verbose_name = _("Space Settings")
+        verbose_name_plural = _("Space Settings")
