d6f8bc11c3ceadffa6d8f73b99c7ff3491962522
Marek Belski <marek@webounty.com>
2016-02-27 21:49:14 -0800

    Darker Style - Ordering of the includes in settings mattered for my templates to override the rest_framework ones. Ended up going with a Slate theme.

--- silent_night/settings.py
+++ silent_night/settings.py
@@ -37,11 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
     'profile_api',
     'game_api',
     'numpy',
     'pymunk',
+    'rest_framework',
 ]
 
 MIDDLEWARE_CLASSES = [
